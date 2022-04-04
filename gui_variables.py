a8fb36e696b2ce3f54ef4c47ea6140edaec1e839
aharbin5 <102874910+aharbin5@users.noreply.github.com>
2022-04-04 17:50:09 -0400

    Add files via upload

---
+++ gui_variables.py
@@ -0,0 +1,7 @@
+battery_percentage = 50
+KW_usage = 833
+mph = 80
+temperature = 90.8
+amps = 200.6
+volts = 50.1
+mph_num = 70
---
+++ parsed_can_message.py
@@ -0,0 +1,18 @@
+pack_current = 0
+pack_inst_voltage = 1
+pack_SOC = 2
+relay_state = 3
+
+high_temp = 4
+low_temp = 5
+avg_temp = 6
+high_temp_id = 7
+low_temp_id = 8
+
+low_cell_voltage = 9
+high_cell_voltage = 9
+avg_cell_voltage = 3
+current_ADC1 = 4
+current_ADC2 = 5
+min_cell_voltage = 6
+max_cell_voltage = 7
