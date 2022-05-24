80b4e75acb7d07ac851af2a1729dab6dab9bace1
KSUSolar <44011160+KSUSolar@users.noreply.github.com>
2022-05-24 18:08:57 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -3,123 +3,33 @@ from PyQt5 import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
-from lightThread import runningLights
 import RPi.GPIO as GPIO
 import threading
 import time
+import os
 from gpiozero import CPUTemperature
-
-global rightLight
-global leftLight
-
-rightLight = False
-leftLight = False
-
-def runningLights(leftLight, rightLight):
-    rightPin = 5
-    leftPin = 6
-
-    rightIn = 17
-    leftIn = 27
-    hazardIn = 22
-    
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(5, GPIO.OUT)
-    GPIO.setup(6, GPIO.OUT)
-    GPIO.output(5, 0)
-    GPIO.output(6, 0)
-    print("GPIO output setup")
-
-    GPIO.setup(rightIn, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    GPIO.setup(leftIn, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    GPIO.setup(hazardIn, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    print("GPIO input setup")
-
-    while (True):
-        if (GPIO.input(hazardIn) == 1):
-            GPIO.output(rightPin, GPIO.HIGH)
-            GPIO.output(leftPin, GPIO.HIGH)
-            rightLight = True
-            leftLight = True
-            time.sleep(0.5)
-            GPIO.output(rightPin, GPIO.LOW)
-            GPIO.output(leftPin, GPIO.LOW)
-            rightLight = False
-            leftLight = False
-            time.sleep(0.5)
-            print('Hazards')
-        else:
-            if (GPIO.input(rightIn) == 1):
-                GPIO.output(rightPin, GPIO.HIGH)
-                rightLight = True
-                time.sleep(0.5)
-                GPIO.output(rightPin, GPIO.LOW)
-                rightLight = False
-                time.sleep(0.5)
-            else:
-                if (GPIO.input(leftIn) == 1):
-                    GPIO.output(leftPin, GPIO.HIGH)
-                    leftLight = True
-                    time.sleep(0.5)
-                    GPIO.output(leftPin, GPIO.LOW)
-                    leftLight = False
-                    time.sleep(0.5)
 
 class MyMainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.time = QTime.currentTime()
-        self.setStyleSheet("background-color: black;")
-        self.setCursor(QCursor(Qt.CrossCursor))
-        self.initUI()
+        global mph_num
+        global leftLight
+        global rightLight
 
-    def initUI(self):
-        # set central widget
-        self.centralGrid = MainGrid()
-        self.setCentralWidget(self.centralGrid)
-
-        # time is shown in status bar
-        self.statusbar = self.statusBar()
-        self.statusbar.showMessage(self.time.toString(Qt.DefaultLocaleLongDate))
-        self.statusbar.setStyleSheet('background-color: orange;')
-        # Alternative color for the time bar = #FF226A.  It's wonderful.
-        
-        # MyMainWindow attributes
-        self.setWindowTitle("SVT KSR1")
-        self.showFullScreen()
-        self.show()
-        
-    def keyPressEvent(self, event):
-        if event.key() == Qt.Key_Escape and self.isFullScreen():
-            self.showNormal()
-            event.accept()
-        elif event.key() == Qt.Key_Escape and (not self.isFullScreen()):
-            self.showFullScreen()
-            event.accept()
-
-class MainGrid(QWidget):
-    def __init__(self):
         super().__init__()
         self.beans = [0, 1, 2, 3, 4, 5, 6, 0]
         self.state_of_charge = self.beans[2]
         self.temperature = self.beans[5]
         self.amps = self.beans[3]
         self.volts = self.beans[1]
-        self.mph_num = self.beans[0]
         self.auxV = self.beans[4]
         self.arTemp = self.beans[6]
         self.pTemp = CPUTemperature().temperature
-        
-        self.lightsThread = threading.Thread(target=runningLights, args=(leftLight, rightLight))
-        self.lightsThread.start()
-        
-        self.initGrid()
 
-    def initGrid(self):
         # setting grid layout which separates widget by row and column
         # QGrid is the layout for the application window
-        grid = QGridLayout()
-        self.setLayout(grid)
+        self.centralGrid = QGridLayout()
+        self.setLayout(self.centralGrid)
 
         self.SOC = QLabel('SOC: ' + str(self.state_of_charge))
         self.SOC.setStyleSheet('background-color: transparent;'
@@ -131,14 +41,14 @@ class MainGrid(QWidget):
 
         # Set left blinker icon
         self.Lblinkers = QLabel(self)
-        pixmap = QPixmap('GUI_Images/icons8-arrow-pointing-left-96.png')
-        self.Lblinkers.setPixmap(pixmap)
+        self.Lblinkers.setText('⬅') # ◄ or ← or ⬅ # Boy do I love ASCII
+        self.Lblinkers.setStyleSheet('color: green; font-size: 90px')
         self.Lblinkers.setAlignment(Qt.AlignCenter)
 
         # Set right blinker icon
         self.Rblinkers = QLabel(self)
-        pixmap = QPixmap('GUI_Images/icons8-arrow-pointing-right-96.png')
-        self.Rblinkers.setPixmap(pixmap)
+        self.Rblinkers.setText('➡')
+        self.Rblinkers.setStyleSheet('color: green; font-size: 90px')
         self.Rblinkers.setAlignment(Qt.AlignCenter)
 
         # Create the warning labels box
@@ -153,7 +63,7 @@ class MainGrid(QWidget):
         mphLabel.setStyleSheet('font-family: verdana,geneva,sans-serif;' 'font-size: 40px;' 'color: #808080;')
         mphLabel.setAlignment(Qt.AlignHCenter)
         
-        self.mphDisp = QLabel(str(self.mph_num))
+        self.mphDisp = QLabel(str(mph_num))
         self.mphDisp.setStyleSheet('color:#FFFFFF;' 'font-family:tahoma,geneva,sans-serif;' 'font-size:40px;' 'font-weight: bold;')
         self.mphDisp.setAlignment(Qt.AlignHCenter)
         
@@ -183,34 +93,34 @@ class MainGrid(QWidget):
         # Create the box for  carStats and its labels & images
         self.carStats = QGroupBox()
         
-        sinI = QLabel(self)
+        sinI = QLabel(self) ### Remove before flight ###
         pixmap2 = QPixmap('GUI_Images/icons8-sine-50.png')
         sinI.setPixmap(pixmap2)
 
-        self.sinLabel = QLabel(str(self.amps) + ' A')
+        self.sinLabel = QLabel('Main: ' + str(self.amps) + ' A')
         self.sinLabel.setStyleSheet('color: white; font-size:20px; font-family:tahoma,geneva,sans-serif;')
 
-        eleI = QLabel(self)
+        eleI = QLabel(self) ### Remove before flight ###
         pixmap3 = QPixmap('GUI_Images/icons8-main-electricity-52.png')
         eleI.setPixmap(pixmap3)
 
-        self.eleLabel = QLabel(str(self.volts) + ' V')
+        self.eleLabel = QLabel('Main: ' + str(self.volts) + ' V')
         self.eleLabel.setStyleSheet('color: white; font-size:20px; font-family:tahoma,geneva,sans-serif;')
 
-        auxI = QLabel(self)
+        auxI = QLabel(self) ### Remove before flight ###
         pixmap4 = QPixmap('GUI_Images/icons8-electricity-52.png')
         auxI.setPixmap(pixmap4)
         
-        self.auxLabel = QLabel(str(self.auxV) + ' V')
+        self.auxLabel = QLabel('Aux: ' + str(self.auxV) + ' V')
         self.auxLabel.setStyleSheet('color: white; font-size:20px; font-family:tahoma,geneva,sans-serif;')
         
         statBox = QGridLayout()
-        statBox.addWidget(sinI, 1, 0)
-        statBox.addWidget(self.sinLabel, 1, 1)
-        statBox.addWidget(eleI, 0, 0)
-        statBox.addWidget(self.eleLabel, 0, 1)
-        statBox.addWidget(auxI, 2, 0)
-        statBox.addWidget(self.auxLabel, 2, 1)
+        #statBox.addWidget(sinI, 1, 0)
+        statBox.addWidget(self.sinLabel, 1, 0)
+        #statBox.addWidget(eleI, 0, 0)
+        statBox.addWidget(self.eleLabel, 0, 0)
+        #statBox.addWidget(auxI, 2, 0)
+        statBox.addWidget(self.auxLabel, 2, 0)
         self.carStats.setLayout(statBox)
 
         # F N R
@@ -236,16 +146,16 @@ class MainGrid(QWidget):
         
         # Placing all the widgets onto the main grid to display
         # column, row, rowspan,columnspan
-        grid.addWidget(self.SOC, 1, 0)
-        grid.addWidget(self.Lblinkers, 0, 0)
-        grid.addWidget(self.Rblinkers, 0, 2)
-        grid.addWidget(self.warnings, 2, 1)
-        grid.addWidget(self.mph, 1, 1)
-        grid.addWidget(self.carStats, 1, 0)
-        grid.addWidget(self.tempSection, 1, 2)
-        grid.addWidget(self.FNR, 0, 1)
+        self.centralGrid.addWidget(self.SOC, 1, 0)
+        self.centralGrid.addWidget(self.Lblinkers, 0, 0)
+        self.centralGrid.addWidget(self.Rblinkers, 0, 2)
+        self.centralGrid.addWidget(self.warnings, 2, 1)
+        self.centralGrid.addWidget(self.mph, 1, 1)
+        self.centralGrid.addWidget(self.carStats, 1, 0)
+        self.centralGrid.addWidget(self.tempSection, 1, 2)
+        self.centralGrid.addWidget(self.FNR, 0, 1)
                 
-        # make QTimer
+        # make QTimer for everything else
         self.qTimer = QTimer(self)
         # set interval to 1 s
         self.qTimer.setInterval(1000) # 1000 ms = 1 s
@@ -253,27 +163,58 @@ class MainGrid(QWidget):
         self.qTimer.timeout.connect(self.getSensorValue)
         # start timer
         self.qTimer.start()
-
+        
+        # make QTimer for MPH
+        self.qTimer2 = QTimer(self)
+        # set interval to 1 s
+        self.qTimer2.setInterval(250) # 1000 ms = 1 s
+        # connect timeout signal to signal handler
+        self.qTimer2.timeout.connect(self.getMPH)
+        # start timer
+        self.qTimer2.start()
+        
+        self.mainWidget = QGroupBox()
+        self.mainWidget.setLayout(self.centralGrid)
+        self.mainWidget.setStyleSheet("background-color: black;")
+        self.mainWidget.setCursor(QCursor(Qt.CrossCursor))
+        self.setCentralWidget(self.mainWidget) ###################################
+        
+        # MyMainWindow attributes
+        self.setWindowTitle("SVT KSR1")
+        self.showFullScreen()
+        self.show()
+        
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_Escape and self.isFullScreen():
+            self.showNormal()
+            event.accept()
+        elif event.key() == Qt.Key_Escape and (not self.isFullScreen()):
+            self.showFullScreen()
+            event.accept()
+        # elif (event.key() == Qt.Key_Q): # This is supposed to be able to kill the canBus & blinker threads but it just freezes the program.  I'm confused.
+            # canThread.join()
+            # lightT.join()
+            
     def getSensorValue(self):
         # beans = recieve_message()
         # beans = [0, 1, 2, 3, 4, 5, 6]
         
-        if (False):
+        if (True):
             self.warnings.setStyleSheet('background-color: red; font-size: 25px;')
         
         self.state_of_charge += 1 # = beans[2]
         self.temperature += 1 # = beans[5]
         self.amps += 2 # = beans[3]
         self.volts += 3 # = beans[1]
-        self.mph_num += 1 # = beans[0]
+        #mph_num += 1 # = beans[0]
         self.auxV += 1 # = beans[4]
 
         self.SOC.setText('SOC: %d' %self.state_of_charge)
-        self.mphDisp.setText(str(self.mph_num))
+        #self.mphDisp.setText(str(mph_num))
         self.battTemp.setText('Batt: ' + str(self.temperature) + '°C')
-        self.sinLabel.setText(str(self.amps) + ' A')
-        self.eleLabel.setText(str(self.volts) + ' V')
-        self.auxLabel.setText(str(self.auxV) + ' V')
+        self.sinLabel.setText('Main: ' + str(self.amps) + ' A')
+        self.eleLabel.setText('Main: ' + str(self.volts) + ' V')
+        self.auxLabel.setText('Aux: ' + str(self.auxV) + ' V')
         self.piTemp.setText('Pi: ' + str(CPUTemperature().temperature) + ' °C')
         self.arrayTemp.setText('Array: ' + str(self.arTemp) + ' °C')
         
@@ -291,20 +232,102 @@ class MainGrid(QWidget):
             self.beans[7] = 0
         else:
             self.beans[7] += 1
-            
-        if (rightLight):
+    
+    def getMPH(self):
+        self.mphDisp.setText(str(mph_num))
+        if (leftLight & rightLight):
+            self.Lblinkers.show()
             self.Rblinkers.show()
+        elif (leftLight):
+            self.Lblinkers.show()
+            self.Rblinkers.hide()
+        elif (rightLight):
+            self.Rblinkers.show()
+            self.Lblinkers.hide()
         else:
             self.Rblinkers.hide()
-        if (leftLight):
-            self.Lblinkers.show()
-        else:
             self.Lblinkers.hide()
-            
-        print('L: ' + str(leftLight) + ' R: ' + str(rightLight))
+
+class canBusThread(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    def run(self):
+        global mph_num
+
+        while (True):
+            mph_num += 1
+            time.sleep(0.4)
+
+class lightThread(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+    
+    def run(self):
+        global leftLight
+        global rightLight
+        
+        rightPin = 5
+        leftPin = 6
+
+        rightIn = 17
+        leftIn = 27
+        hazardIn = 22
+        
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(5, GPIO.OUT)
+        GPIO.setup(6, GPIO.OUT)
+        GPIO.output(5, 0)
+        GPIO.output(6, 0)
+        print("GPIO output setup")
+
+        GPIO.setup(rightIn, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.setup(leftIn, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.setup(hazardIn, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        print("GPIO input setup")
+
+        while (True):
+            if (GPIO.input(hazardIn) == 1):
+                GPIO.output(rightPin, GPIO.HIGH)
+                GPIO.output(leftPin, GPIO.HIGH)
+                rightLight = True
+                leftLight = True
+                time.sleep(0.5)
+                GPIO.output(rightPin, GPIO.LOW)
+                GPIO.output(leftPin, GPIO.LOW)
+                rightLight = False
+                leftLight = False
+                time.sleep(0.5)
+            else:
+                if (GPIO.input(rightIn) == 1):
+                    GPIO.output(rightPin, GPIO.HIGH)
+                    rightLight = True
+                    time.sleep(0.5)
+                    GPIO.output(rightPin, GPIO.LOW)
+                    rightLight = False
+                    time.sleep(0.5)
+                else:
+                    if (GPIO.input(leftIn) == 1):
+                        GPIO.output(leftPin, GPIO.HIGH)
+                        leftLight = True
+                        time.sleep(0.5)
+                        GPIO.output(leftPin, GPIO.LOW)
+                        leftLight = False
+                        time.sleep(0.5)
 
 if __name__ == '__main__':
-    import sys
+    mph_num = 1
+    leftLight = False
+    rightLight = False
+    
+    os.system('sudo ip link set can0 type can bitrate 250000') # This sets up the canBus interface
+    os.system('sudo ifconfig can0 up') # This starts said canBus interface
+    
+    canThread = canBusThread()
+    canThread.start()
+    
+    lightT = lightThread()
+    lightT.start()
 
     app = QApplication(sys.argv)
     ex = MyMainWindow()
